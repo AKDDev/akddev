9cb0def173e14f37a0e6b3c6ac54b2d7185e93ec
matalina <matalina@gmail.com>
2020-09-25 16:22:02 +0000

    stubbed out portfolio controller

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -4,47 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Models\Portfolio;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StorePortfolioRequest;
+use App\Http\Requests\StorePorfolioRequest;
 use App\Http\Requests\UpdatePorfolioRequest;
-use App\Http\Resources\Portfolio as PortfolioResource;
 
 class PortfolioController extends Controller
 {
     public function index()
     {
         $projects = Portfolio::active()->get();
-        
-        return PortfolioResource($projects);
+
+        return view('portfolio.list')
+            ->with('projects', $projects);
     }
 
 
     public function create()
     {
-        //
+        return view('portfolio.create');
     }
 
-    public function store(StorePortfolioRequest $request)
+    public function store(StorePorfolioRequest $request)
     {
-        //
+        Portfolio::create([
+            'project' => $request->get('project')
+        ]);
+
+        session()->flash('success','Portfolio project created.');
+
+        return redirect()->route('portfolio');
     }
 
     public function show(Portfolio $portfolio)
     {
-        //
+        return view('portfolio.show')
+            ->with('portfolio',$portfolio);
     }
 
     public function edit(Portfolio $portfolio)
     {
-        //
+        return view('portfolio.edit')
+            ->with('portfolio',$portfolio);
     }
 
     public function update(UpdatePorfolioRequest $request, Portfolio $portfolio)
     {
-        //
+        $portfolio->project = $request->get('project');
+
+        $portfolio->save();
+
+        session()->flash('success','Portfolio project updated.');
+
+        return redirect()->route('portfolio');
     }
 
     public function destroy(Portfolio $portfolio)
     {
-        //
+        $portfolio->delete();
+
+        session()->flash('success','Portfolio project deleted.');
+
+        return redirect()->route('portfolio');
+
     }
 }
